95decaffb3e91c4c63372710d263db57e5e87ef4
Dmitrii Malykh <malyh92@mail.ru>
2023-10-17 23:00:28 +0400

    feat: js код по модулям

---
+++ src/components/modal.js
@@ -0,0 +1,71 @@
+import {addCard, createCard} from "./card";
+
+const profileBlock = document.querySelector('.popup-profile');
+export const profileForm = profileBlock.querySelector('.popup__form');
+const popupProfileUsername = profileForm.querySelector('.popup__input-username');
+const popupProfileJob = profileForm.querySelector('.popup__input-job');
+
+const element = document.querySelector('.popup-new-card');
+export const newCardForm = element.querySelector('.popup__form');
+const nameNewCard = newCardForm.querySelector('.popup__input-name');
+const linkNewCard = newCardForm.querySelector('.popup__input-link');
+
+const popupImage = document.querySelector('.popup-image');
+const popupImageImg = popupImage.querySelector('.popup__image');
+const popupImageCaption = popupImage.querySelector('.popup__image-caption');
+
+const profileInfo = document.querySelector('.profile > .profile__info');
+const profileCaption = profileInfo.querySelector('.profile__caption');
+const profileTitle = profileCaption.querySelector('.profile__title');
+const profileJob = profileInfo.querySelector('.profile__job');
+export const profileEditBtn = profileCaption.querySelector('.profile__edit-button');
+
+export const popups = document.querySelectorAll('.popup');
+export const popupCloseButtons = document.querySelectorAll('.popup__close-button');
+
+export const handleNewCardButtonClick = () => openPopup(element);
+
+export const handlePopupNewCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  addCard(createCard(nameNewCard.value, linkNewCard.value));
+  closePopup(element);
+  resetForm(newCardForm);
+};
+
+export const handlePopupProfileFormSubmit = (evt) => {
+  evt.preventDefault();
+  profileTitle.textContent = popupProfileUsername.value;
+  profileJob.textContent = popupProfileJob.value;
+  closePopup(profileBlock);
+};
+
+export const handleProfileEditBtnClick = () => {
+  popupProfileUsername.value = profileTitle.textContent;
+  popupProfileJob.value = profileJob.textContent;
+  openPopup(profileBlock);
+};
+
+export const handlePopupClose = (evt) => {
+  const popup = evt.currentTarget.classList.contains("popup") ? evt.target : evt.target.closest('.popup');
+  if (popup) closePopup(popup);
+};
+
+
+export function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+export function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+export function initImagePopup(name, link) {
+  popupImageImg.src = link;
+  popupImageImg.alt = name;
+  popupImageCaption.textContent = name;
+  openPopup(popupImage);
+}
+
+function resetForm(form) {
+  form.reset();
+}
