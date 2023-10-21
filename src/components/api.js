eaa7b0cb98e581588df780dfeae30e84669928cb
Dmitrii Malykh <malyh92@mail.ru>
2023-10-21 10:56:37 +0400

    feat: init api

---
+++ src/components/api.js
@@ -0,0 +1,9 @@
+const config = {
+  baseUrl: 'https://nomoreparties.co/v1/wbf-cohort-13',
+  headers: {
+    authorization: 'd870b268-feb8-4751-8894-514caff2a776',
+    'content-type': 'application/json'
+  }
+}
+
+
